763d144363b5e9fb71f961fb940b7566821049f7
cowthatprograms <cowthatprograms@gmail.com>
2020-09-04 12:52:30 -0400

    port

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// packages
 const http = require('http');
 
+// to work locally
+let port = process.env.PORT;
+if (port == null || port == "") { port = 8080; }
+
+// main
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('LegendCow');
   res.end();
-}).listen(process.env.PORT);
+}).listen(port);
